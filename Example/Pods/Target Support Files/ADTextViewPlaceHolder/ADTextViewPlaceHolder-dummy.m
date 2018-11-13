5b776653485f24e1b10963289ce955be9452fa42
liushuo <liushuo@alaxiaoyou.com>
2018-11-13 14:13:23 +0800

    Initial Commit

---
+++ Example/Pods/Target Support Files/ADTextViewPlaceHolder/ADTextViewPlaceHolder-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ADTextViewPlaceHolder : NSObject
+@end
+@implementation PodsDummy_ADTextViewPlaceHolder
+@end
---
+++ Example/Pods/Target Support Files/Pods-ADTextViewPlaceHolder_Tests/Pods-ADTextViewPlaceHolder_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ADTextViewPlaceHolder_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_ADTextViewPlaceHolder_Tests
+@end
